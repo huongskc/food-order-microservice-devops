bdbac0d7afbdb82c2a5d776b95cea049479b8a18
huongskc <huong2308skc@gmail.com>
2026-08-18 20:11:58 +0700

    fix(api-gateway): handle all HTTP methods in fallback and add timelimiter

--- api-gateway/src/main/java/com/vanhuy/api_gateway/controller/FallbackController.java
+++ api-gateway/src/main/java/com/vanhuy/api_gateway/controller/FallbackController.java
@@ -1,22 +1,22 @@
 package com.vanhuy.api_gateway.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallbackController {
-    @GetMapping("/fallback/restaurant")
+    @RequestMapping("/fallback/restaurant")
     public ResponseEntity<String> restaurantFallback() {
         return ResponseEntity.ok("Restaurant Service is currently unavailable. Please try again later.");
     }
 
-    @GetMapping("/fallback/order")
+    @RequestMapping("/fallback/order")
     public ResponseEntity<String> orderFallback() {
         return ResponseEntity.ok("Order Service is currently unavailable. Please try again later.");
     }
 
-    @GetMapping("/fallback/user")
+    @RequestMapping("/fallback/user")
     public ResponseEntity<String> userFallback() {
         return ResponseEntity.ok("User Service is currently unavailable. Please try again later.");
     }
